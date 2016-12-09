2fce3e3085c05920565799eeb9fa3670236d6fe2
Balamurali Pandranki <balamurali@live.com>
2016-12-09 21:55:44 +0530

    Working barebones

--- main.go
+++ main.go
@@ -1,11 +1,22 @@
 package main
 
-import "gopkg.in/kataras/iris.v4"
+import (
+	"gopkg.in/kataras/iris.v4"
+	"os"
+	"github.com/kataras/go-template/html"
+)
 
 func main(){
+
+	port := os.Getenv("PORT")
+
+	if port == "" {
+		port = "8080"
+	}
 	api := iris.New()
+	api.UseTemplate(html.New()).Directory("./public/templates", ".html")
 	api.Get("/", func(ctx  *iris.Context) {
 		ctx.Render("home.html", struct { Name string }{ Name: "GopherHack Hyderabad" })
 	})
-	api.Listen(":8080")
+	api.Listen(":"+port)
 }
